92f3f848634428d752e012bcda84397699c615a5
laudeon <louis.audeon@mail.be>
2018-03-06 21:59:06 +0100

    add possibility to save the tree in a file as json

--- index.js
+++ index.js
@@ -5,28 +5,32 @@
 
 require('dotenv').config()
 
+const fs = require('fs')
+const path = require('path')
 const debug = require('debug')('drive-list-folder')
 const app = require('./lib')
-const {consoleTree, getTree} = require('./lib/tree')
+const {getTree} = require('./lib/tree')
 
-// const params = {
-//   supportsTeamDrives: true,
-//   corpora: 'teamDrive',
-//   includeTeamDriveItems: true,
-//   teamDriveId: process.env.TEAM_DRIVE_ID,
-//   pageSize: 1000,
-//   q: `mimeType = 'application/vnd.google-apps.folder'`,
-//   fields: 'files(parents,name,id)'
-// }
-
-exports.getTree = (params) => {
+const tree = (params) => {
   return app.vroum(params)
     .then(res => getTree(res.files))
     .catch(debug)
 }
 
-exports.consoleTree = (params) => {
+const treeToFile = (params, path) => {
   return app.vroum(params)
-    .then(res => consoleTree(res.files))
+    .then(res => {
+      const tree = getTree(res.files)
+      const data = JSON.stringify(tree)
+      fs.writeFile(path, data, 'UTF-8', err => {
+        if (err) throw err
+        debug('tree saved in json file!')
+      })
+    })
     .catch(debug)
+}
+
+module.exports = {
+  tree: tree,
+  treeToFile: treeToFile
 }
